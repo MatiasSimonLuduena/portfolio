e592c286e8f7debc31ceb199e4065754ce15a2cb
MatiasSimonLuduena <matipax04@gmail.com>
2023-10-29 00:13:35 -0300

    home

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { useState } from "react"
 // imports components
 import Navbar from "./components/navbar/Navbar"
 import SliderMenu from "./components/navbar/SliderMenu"
-import Footer from "./components/footer/Footer";
+import Home from "./components/home/Home"
+import Footer from "./components/footer/Footer"
 
 function App() {
   const [slider, setSlider] = useState(null);
@@ -12,6 +13,7 @@ function App() {
     <>
       <SliderMenu setSlider={setSlider} slider={slider} />
       <Navbar setSlider={setSlider} />
+      <Home />
       <Footer />
     </>
   )
